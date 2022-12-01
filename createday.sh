4055e5cb6b5aeb6b52a665ec2b32ebb16ccac5f6
Tim Quelch <timothy.quelch@versent.com.au>
2022-12-01 16:33:35 +1100

    Fix createday to pad with 0s

--- createday.sh
+++ createday.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 day=$1
-cp "src/template.jl"  "src/d${day}.jl"
-sed -i "s/DAYCODE/d${day}/g" "src/d${day}.jl"
-sed -i "s/days = \[\(.*\)\]/days = \[\1, ${day}\]/g" "src/AdventOfCode.jl"
-aocd "${day}" > "data/d${day}.txt"
+daystr=$(printf "%02d" $1)
+cp "src/template.jl"  "src/d${daystr}.jl"
+sed -i "s/DAYCODE/d${daystr}/g" "src/d${daystr}.jl"
+sed -i "s/days = \[\(.*\)\]/days = \[\1, ${daystr}\]/g" "src/AdventOfCode.jl"
+aocd "${day}" > "data/d${daystr}.txt"
