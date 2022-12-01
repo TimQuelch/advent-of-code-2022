aea60a40957bf091158f8992b928896c67a79b7e
Tim Quelch <timothy.quelch@versent.com.au>
2022-12-02 10:49:02 +1100

    Don't add leading zeros to array

--- createday.sh
+++ createday.sh
@@ -4,5 +4,5 @@ day=$1
 daystr=$(printf "%02d" $1)
 cp "src/template.jl"  "src/d${daystr}.jl"
 sed -i "s/DAYCODE/d${daystr}/g" "src/d${daystr}.jl"
-sed -i "s/days = \[\(.*\)\]/days = \[\1, ${daystr}\]/g" "src/AdventOfCode.jl"
+sed -i "s/days = \[\(.*\)\]/days = \[\1, ${day}\]/g" "src/AdventOfCode.jl"
 aocd "${day}" > "data/d${daystr}.txt"
